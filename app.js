3db55f824a72e327970caf265c4208eec59c3480
Vincent Mangelschots <v.mangelschots@xpofleet.com>
2019-09-18 16:47:48 +0200

    logs toegevoegd

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.set('view engine', 'ejs');
 
 app.use(favicon(path.join(__dirname, 'public', 'img', 'favicon.png')));
 app.set('port', process.env.PORT);
-//app.use(logger('dev'));
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
